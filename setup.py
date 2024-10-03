a9be95e378f9d8a998d4bc972dae576264e0fa31
alexeiplatzer <109217496+alexeiplatzer@users.noreply.github.com>
2024-10-03 15:44:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from setuptools import find_packages
-from distutils.core import setup
+from setuptools import find_packages, setup
+# from distutils.core import setup
 
 setup(
     name='go2_gym',
@@ -9,13 +9,13 @@ setup(
     packages=find_packages(),
     author_email='540614105@qq.com',
     description='Toolkit for deployment of sim-to-real RL on the Unitree Go2.',
-    install_requires=['ml_logger==0.8.117',
-                      'ml_dash==0.3.20',
+    install_requires=['ml_logger>=0.8.117',
+                      'ml_dash>=0.3.20',
                       'jaynes>=0.9.2',
-                      'params-proto==2.10.5',
+                      'params-proto>=2.10.5',
                       'gym>=0.14.0',
                       'tqdm',
                       'matplotlib',
-                      'numpy==1.23.5'
+                      'numpy>=1.23.5'
                       ]
 )
